99dd12519c36f236de34dac3588e706f208255f0
ElisIrons <elisirons96@gmail.com>
2024-04-24 17:49:11 -0300

    Refatoração

--- src/main/java/com/soulcode/sistemaTI/Controllers/UsuarioController.java
+++ src/main/java/com/soulcode/sistemaTI/Controllers/UsuarioController.java
@@ -4,9 +4,7 @@ import com.soulcode.sistemaTI.Models.ChamadosModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +12,14 @@ import java.util.List;
 @Controller
 public class UsuarioController {
 
-    private List<ChamadosModel> chamados = new ArrayList<>();
-
-    @GetMapping("/login-usuario")
+    @RequestMapping(value = "/login-usuario", method = RequestMethod.GET)
     public String loginUsuario() {
         return "login-usuario";
     }
 
-    @PostMapping("/login-usuario")
+   @RequestMapping(value = "/login-usuario", method = RequestMethod.POST)
     public String loginUsuario(@RequestParam String name, Model model) {
-        model.addAttribute("name", name);
+        model.addAttribute( "name", name);
 
         return "redirect:/chamados-sistema?name=" + name;
     }
